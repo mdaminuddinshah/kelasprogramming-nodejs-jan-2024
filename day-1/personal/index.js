048df5ca7e8b3a12d89176e843db48e1ed202519
mdaminuddinshah <muhammadaminuddinshah98@gmail.com>
2024-03-18 14:38:14 +0800

    test node index.js

---
+++ day-1/personal/index.js
@@ -0,0 +1,23 @@
+let a = 'amin'
+a = 'aminuddin'
+
+const b = 'shah'
+
+const fullname = `His name is ${a} ${b}`
+console.log(fullname)
+
+const array1 = [
+    "👻","☠️","🤖","🤠","💀","👽","👾","🤖",
+    "⚽","⚾","🏀","🥎","🎳","🎮","🎃","🎃"
+]
+// console.log(array1.length) = 16
+for(let i = 0; i < array1.length; i+=1){
+    console.log(i);
+    console.log(array1[i]);
+    // console.log(`index of array1 ${array1[i]} is ${i}`)
+}
+
+const firstname = "aminuddin"
+const lastname = "masurom"
+const fullname2 = `my name is ${firstname} ${lastname}`
+console.log(fullname2)
